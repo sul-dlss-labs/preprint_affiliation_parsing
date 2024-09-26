848fd8ec6c826d40edc9ce335f3ea7dadd98e2a1
Nick Budak <budak@stanford.edu>
2024-09-26 13:27:55 -0700

    Extraction dataset creation workflow

--- scripts/clean_dataset.py
+++ scripts/clean_dataset.py
@@ -1,19 +1,29 @@
-# !/usr/bin/env python
+#!/usr/bin/env python
 
-import contextlib
 import pathlib
 import re
 import unicodedata
 
+import pymupdf
 import pypdf
 import typer
 from rich import print
 from rich.progress import track
 
 
-def extract_text_from_pdf(path) -> str:
+def extract_text_pymupdf(path) -> str:
+    doc = pymupdf.open(path)
+    return "\n".join(page.get_text() for page in doc)
+
+
+def extract_text_pypdf(path) -> str:
     reader = pypdf.PdfReader(path)
-    return " ".join([page.extract_text() for page in reader.pages])
+    return "\n".join(page.extract_text() for page in reader.pages)
+
+
+def remove_numbered_lines(text: str) -> str:
+    lines = text.splitlines()
+    return "\n".join([line for line in lines if not re.match(r"^\d+\W*$", line)])
 
 
 def normalize_whitespace(text) -> str:
@@ -26,37 +36,28 @@ def normalize_unicode(text) -> str:
 
 
 def normalize(text) -> str:
-    return normalize_unicode(normalize_whitespace(text))
+    return normalize_unicode(normalize_whitespace(remove_numbered_lines(text)))
 
 
-def main(input_dir: pathlib.Path, output_dir: pathlib.Path):
+def main(input_dir: pathlib.Path, output_dir: pathlib.Path) -> None:
     """Extract text from all PDFs, normalize, and output to text files."""
+    # Create output directory if it doesn't exist
+    output_dir.mkdir(parents=True, exist_ok=True)
+
     # Clear text files in output directory
     for txt_path in output_dir.glob("*.txt"):
         txt_path.unlink()
 
-    # Create output directory if it doesn't exist
-    output_dir.mkdir(parents=True, exist_ok=True)
-
     # Extract text from each PDF and write to text file in output directory
-    pdf_paths = list(sorted(input_dir.glob("*.pdf")))
+    pdf_paths = list(sorted(input_dir.glob("**/*.pdf")))
     total = 0
     for pdf_path in track(pdf_paths, description="Extracting text..."):
         output_path = pathlib.Path(output_dir, f"{pdf_path.stem}.txt")
-
-        # Suppress warnings from pypdf
-        with contextlib.redirect_stderr(None):
-            contents = extract_text_from_pdf(pdf_path)
-
-        # If text extraction didn't yield anything, warn and move on
-        normalized = normalize(contents)
-        if not normalized:
-            print(f"[red]Failed to extract text from {pdf_path}.[/red]")
-            continue
-
-        # Write normalized text to output file
-        output_path.write_text(normalize(contents))
-        total += 1
+        try:
+            output_path.write_text(normalize(extract_text_pymupdf(pdf_path)))
+            total += 1
+        except Exception as e:
+            print(f"Error extracting text from {pdf_path}: {e}")
 
     print(f"Extracted text from {total} PDFs.")
 
---
+++ scripts/download_dataset.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+
+import csv
+import pathlib
+
+import requests
+import typer
+from rich import print
+from rich.progress import track
+
+# Column names in the input spreadsheet
+ID_COLUMN = "OpenAlex ID"
+DRUID_COLUMN = "DRUID"
+
+
+def download_file(url: str, output_path: pathlib.Path) -> None:
+    """Download file from URL and save to output path."""
+    response = requests.get(
+        url, allow_redirects=True, headers={"Accept": "application/pdf"}
+    )
+    response.raise_for_status()
+    output_path.write_bytes(response.content)
+
+
+def main(input_file: pathlib.Path, output_dir: pathlib.Path) -> None:
+    """Download all PDF files in input spreadsheet to output directory."""
+    # Create output directory if it doesn't exist
+    output_dir.mkdir(parents=True, exist_ok=True)
+
+    # Clear pdf files in output directory
+    for pdf_path in output_dir.glob("*.pdf"):
+        pdf_path.unlink()
+
+    # Download each file listed in the PDF URL column of the input spreadsheet
+    downloaded = 0
+    with open(input_file, "r") as file:
+        reader = csv.DictReader(file)
+        rows = list(reader)
+        for row in track(rows, description="Downloading PDFs...", total=len(rows)):
+            druid = row[DRUID_COLUMN]
+            openalex_id = row[ID_COLUMN].lstrip("https://openalex.org/")
+            pdf_url = f"https://sul-stacks-stage.stanford.edu/file/{druid}/{openalex_id}.pdf"
+            pdf_name = f"{openalex_id}.pdf"
+            pdf_path = pathlib.Path(output_dir, pdf_name)
+            try:
+                download_file(pdf_url, pdf_path)
+                downloaded += 1
+            except Exception as e:
+                print(f"Error downloading {pdf_url}: {e}")
+    print(f"Downloaded {downloaded} PDFs.")
+
+
+if __name__ == "__main__":
+    typer.run(main)
+
+__doc__ = main.__doc__
