884b97e070394e3e3623aec232bfdd6ff683a25b
Nick Budak <budak@stanford.edu>
2024-10-17 09:22:15 -0700

    Add more options and PDF preview to visualizer

--- scripts/visualize_textcat.py
+++ scripts/visualize_textcat.py
@@ -1,9 +1,11 @@
 import pathlib
+import random
 
 import pandas as pd
 import spacy
 import streamlit as st
 from create_parsing_dataset import get_affiliations, is_affiliation
+from streamlit_pdf_viewer import pdf_viewer
 
 if __name__ == "__main__":
     # Better display for longer texts
@@ -14,6 +16,7 @@ if __name__ == "__main__":
 
     # Preload all preprints
     all_preprints = []
+    all_openalex_ids = []
     for file in pathlib.Path("assets/preprints/txt").glob("*.txt"):
         all_preprints.append(
             {
@@ -23,23 +26,33 @@ if __name__ == "__main__":
                 },
             }
         )
+        all_openalex_ids.append(file.stem)
 
-    # Handler to get the preprint text from its ID
-    def get_preprint(openalex_id):
+    # Get the preprint text from its ID
+    def get_preprint_text(openalex_id):
         return next(
-            preprint
+            preprint["text"]
             for preprint in all_preprints
             if preprint["meta"]["openalex_id"] == openalex_id
         )
-    
+
+    # Handler to get a random preprint
+    def random_preprint():
+        st.session_state.selected_preprint = random.choice(all_openalex_ids)
+
+    # Handler to choose a preprint
+    def choose_preprint(openalex_id):
+        st.session_state.selected_preprint = openalex_id
+
     # Columns
     col1, col2, col3 = st.columns([3, 1, 1])
 
     # Dropdown to select a preprint
     with col1:
-        text = st.selectbox(
+        openalex_id = st.selectbox(
             "Select a preprint",
             options=[preprint["meta"]["openalex_id"] for preprint in all_preprints],
+            key="selected_preprint",
         )
 
     # Number input for selecting threshold
@@ -62,29 +75,42 @@ if __name__ == "__main__":
             step=1,
         )
 
+    # Quick-select buttons for some preprints + randomizer
+    st.button("🔄 Random preprint", type="primary", on_click=random_preprint)
+    st.button("#️⃣ Line numbers", on_click=lambda: choose_preprint("W4226140866"))
+    st.button("🗒️ Affiliations as footnotes", on_click=lambda: choose_preprint("W3124742002"))
+
     # Display the selected preprint
-    st.title(text)
+    st.title(openalex_id)
+
+    # More columns
+    col4, col5 = st.columns([1, 1])
 
     # Display the extracted affiliation text
-    text = get_preprint(text)["text"]
-    st.header("Extracted affiliations")
-    affiliations = get_affiliations(text, nlp, window, threshold)
-    st.markdown(f"> {'\n>\n> '.join(affiliations)}")
+    with col4:
+        text = get_preprint_text(openalex_id)
+        st.header("Extracted affiliations")
+        affiliations = get_affiliations(text, nlp, window, threshold)
+        st.markdown(f"> {' '.join(affiliations)}")
 
-    # Display the breakdown of the analyzed chunks
-    chunks = text.split("\n")[:window]
-    chunk_docs = [nlp(chunk) for chunk in chunks]
-    df = pd.DataFrame(
-        [
+        # Display the breakdown of the analyzed chunks
+        chunks = text.split("\n")[:window]
+        chunk_docs = [nlp(chunk) for chunk in chunks]
+        df = pd.DataFrame(
             [
-                doc.text,
-                is_affiliation(doc, threshold),
-                doc.cats["AFFILIATION"],
-                doc.cats["NOT_AFFILIATION"],
-            ]
-            for doc in chunk_docs
-        ],
-        columns=["text", "decision", "AFFILIATION", "NOT_AFFILIATION"],
-    )
-    st.header("Chunk breakdown")
-    st.dataframe(df, use_container_width=True)
+                [
+                    doc.text,
+                    is_affiliation(doc, threshold),
+                    doc.cats["AFFILIATION"],
+                    doc.cats["NOT_AFFILIATION"],
+                ]
+                for doc in chunk_docs
+            ],
+            columns=["text", "decision", "AFFILIATION", "NOT_AFFILIATION"],
+        )
+        st.header("Chunk breakdown")
+        st.dataframe(df, use_container_width=True)
+
+    # Display the pdf
+    with col5:
+        pdf_viewer(f"assets/preprints/pdf/{openalex_id}.pdf", pages_to_render=[1])
