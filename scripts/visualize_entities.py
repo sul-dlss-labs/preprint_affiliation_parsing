c0e90f5db4c2c8c4787d5ad108f5742dc94f749f
Nick Budak <budak@stanford.edu>
2024-10-28 11:25:40 -0700

    Disable entity merging in NER viz

--- scripts/visualize_entities.py
+++ scripts/visualize_entities.py
@@ -2,24 +2,17 @@ import spacy
 import spacy_streamlit
 import spacy_transformers  # required to load transformer models
 import streamlit as st
-from utils import (
-                   add_affiliation_keys,
-                   get_affiliation_keys,
-                   get_affiliations,
-                   load_model,
-)
+from utils import get_affiliations, load_model, set_affiliation_ents
 
 # Load model and run it
 textcat = spacy.load("training/textcat/model-best")
 affiliations = get_affiliations(st.session_state.pdf_text, textcat, 0.75)
 nlp = load_model(st.session_state.ner_model)
+if nlp.has_pipe("merge_entities"):
+    nlp.disable_pipe("merge_entities")
 doc = nlp(affiliations)
-
-# Remove unused NER tags and add affiliation keys
-new_ents = [ent for ent in doc.ents if ent.label_ in ["PERSON", "ORG", "GPE"]]
-doc.ents = new_ents
-keys = get_affiliation_keys(nlp, doc)
-add_affiliation_keys(nlp, doc)
+set_affiliation_ents(nlp, doc)
 
 # Main content area
-spacy_streamlit.visualize_ner(doc, labels=["PERSON", "ORG", "GPE", "KEY"])
+spacy_streamlit.visualize_ner(doc, labels=["PERSON", "ORG", "GPE", "KEY"], show_table=False)
+spacy_streamlit.visualize_tokens(doc)
