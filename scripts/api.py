4f645b7f2281b4f9ddd24876e4b53a089e8f0d7a
Nick Budak <budak@stanford.edu>
2024-11-13 14:54:30 -0800

    Refactor the API
    - Support sending an async or sync file stream
    - Add documentation to the schema
    - Memoize spaCy model loading

--- scripts/api.py
+++ scripts/api.py
@@ -1,29 +1,42 @@
-from tempfile import SpooledTemporaryFile
+import inspect
 
+import networkx as nx
 import spacy
 import spacy_transformers  # noqa: F401
 from clean_preprints import pdf_bytes_to_struct, text_from_struct
 from fastapi import FastAPI, HTTPException, UploadFile
-from pydantic import BaseModel
-from utils import (
-    get_affiliation_graph,
-    get_affiliation_pairs,
-    get_affiliation_text,
-    set_affiliation_ents,
-)
+from pydantic import BaseModel, Field
+from utils import (get_affiliation_graph, get_affiliation_pairs,
+                   get_affiliation_text, set_affiliation_ents)
 
-# Load the models
-ner = spacy.load("en_core_web_trf")
-ner.disable_pipes("parser")
-textcat = spacy.load("training/textcat/model-best")
+ner_model = None
+textcat_model = None
+
+
+# memoized helpers for loading models so we don't have to reload them on every request
+def load_ner_model():
+    global ner_model
+    if ner_model is None:
+        ner_model = spacy.load("en_core_web_trf")
+    return ner_model
+
+
+def load_textcat_model():
+    global textcat_model
+    if textcat_model is None:
+        textcat_model = spacy.load("training/textcat/model-best")
+    return textcat_model
 
 
 # Helper to run the entire processing pipeline on an uploaded file
-async def analyze_pdf(file: SpooledTemporaryFile):
-    pdf_bytes = await file.read()
+async def analyze_pdf(file, textcat, ner, threshold=0.75) -> nx.Graph:
+    if inspect.iscoroutinefunction(file.read):
+        pdf_bytes = await file.read()
+    else:
+        pdf_bytes = file.read()
     pdf_struct = pdf_bytes_to_struct(pdf_bytes)
     pdf_text = text_from_struct(pdf_struct)
-    affiliation_text = get_affiliation_text(pdf_text, textcat, 0.75)
+    affiliation_text = get_affiliation_text(pdf_text, textcat, threshold)
     doc = ner(affiliation_text)
     doc = set_affiliation_ents(ner, doc)
     return get_affiliation_graph(doc)
@@ -35,23 +48,27 @@ app = FastAPI()
 
 
 class Organization(BaseModel):
-    name: str
+    name: str = Field(description="The name of the organization")
 
 
 class Person(BaseModel):
-    name: str
-    affiliation: Organization | None = None
+    name: str = Field(description="The full name of the person")
+    affiliation: Organization | None = Field(
+        None, description="Organization the person is affiliated with, if any"
+    )
 
 
 class Document(BaseModel):
-    authors: list[Person]
+    authors: list[Person] = Field(
+        [], description="List of authors and their affiliations"
+    )
 
 
 @app.post("/analyze")
 async def analyze_file(file: UploadFile) -> Document:
     """
     Analyze a PDF file uploaded as form data.
-    
+
     Usage:
     ```
     curl -F file=@assets/preprints/pdf/W2901173781.pdf "http://localhost:8000/analyze"
@@ -61,8 +78,12 @@ async def analyze_file(file: UploadFile) -> Document:
     if not file.filename.endswith(".pdf"):
         raise HTTPException(status_code=400, detail="File must be in PDF format")
 
+    # Load the models
+    textcat = load_textcat_model()
+    ner = load_ner_model()
+
     # Analyze the document
-    graph = await analyze_pdf(file)
+    graph = await analyze_pdf(file, textcat, ner)
 
     # Format all of the authors & affiliations
     people = []
