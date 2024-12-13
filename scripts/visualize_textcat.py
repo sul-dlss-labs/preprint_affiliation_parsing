e35d2294e4dfe769e8f25756a75aedc32a04204f
Nick Budak <budak@stanford.edu>
2024-12-13 09:37:28 -0800

    Update textcat visualizer for multilabel version

--- scripts/visualize_textcat.py
+++ scripts/visualize_textcat.py
@@ -3,10 +3,10 @@ from itertools import groupby
 import pandas as pd
 import spacy
 import streamlit as st
-from utils import analyze_blocks, get_affiliation_blocks
+from utils import analyze_blocks, get_affiliation_spans
 
 # Load textcat model
-nlp = spacy.load("training/textcat/model-best")
+nlp = spacy.load("training/textcat_multilabel/model-best")
 
 # Main content area
 col1, col2 = st.columns([1, 1])
@@ -15,21 +15,31 @@ col1, col2 = st.columns([1, 1])
 analyzed_blocks = analyze_blocks(
     st.session_state.pdf_text, nlp, st.session_state.threshold
 )
-affiliation_blocks = get_affiliation_blocks(
-    st.session_state.pdf_text, nlp, st.session_state.threshold
-)
+flat_blocks = [block for page in analyzed_blocks for block in page]
+affiliation_blocks = [block for block in flat_blocks if block["is_affiliation"]]
 affiliations = " ".join([block["text"] for block in affiliation_blocks])
-blocks_by_page = groupby(affiliation_blocks, key=lambda block: block["page"])
-pages = [page for page, _ in blocks_by_page]
+spans_by_page = groupby(affiliation_blocks, key=lambda block: block["page"])
+pages = [page for page, _ in spans_by_page]
+
+
+def block_emoji(block):
+    if block["cats"]["AFFILIATION"] >= st.session_state.threshold and block["cats"]["AUTHOR"] >= st.session_state.threshold:
+        return "🟩"
+    elif block["cats"]["AFFILIATION"] >= st.session_state.threshold:
+        return "🟨"
+    elif block["cats"]["AUTHOR"] >= st.session_state.threshold:
+        return "🟦"
+    elif block["cats"]["CITATION"] >= st.session_state.threshold:
+        return "🟥"
+    else:
+        return "⬜"
 
 # Display the block analysis heatmap
 with col1:
     st.header("Block heatmap")
     block_heatmap = []
     for page in analyzed_blocks:
-        block_heatmap.append(
-            [f"{'🟩' if block['is_affiliation'] else '⬜'}" for block in page]
-        )
+        block_heatmap.append([block_emoji(block) for block in page])
     st.text("\n".join(["".join(row) for row in block_heatmap]))
 
 # Display the extracted affiliation text
@@ -48,12 +58,13 @@ df = pd.DataFrame(
             int(block["page"]) + 1,
             block["is_affiliation"],
             block["text"],
-            block["cats"]["AFFILIATION"],
-            block["cats"]["NOT_AFFILIATION"],
+            round(block["cats"]["AFFILIATION"], 2),
+            round(block["cats"]["AUTHOR"], 2),
+            round(block["cats"]["CITATION"], 2),
         ]
-        for block in all_blocks_on_affiliation_pages
+        for block in flat_blocks
     ],
-    columns=["page", "decision", "text", "AFFILIATION", "NOT_AFFILIATION"],
+    columns=["page", "decision", "text", "AFFILIATION", "AUTHOR", "CITATION"],
 )
 st.header("Block breakdown")
 st.dataframe(df, use_container_width=True, hide_index=True)
